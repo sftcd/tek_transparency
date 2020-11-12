c1bd51df3e7e3042f38bbeae363597c8f2b94959
sftcd <stephen@down.dsg.cs.tcd.ie>
2020-11-12 14:14:24 +0000

    added script to merge zips from manual run done elsewhere with all-zips

---
+++ man-merge.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# "manually" merge the files from a run with those in the all-zips
+# directory 
+
+# had a case where the machine running tek_survey.sh was offline for
+# a bit and I manually did a run on a dev laptop to fill in that gap
+
+# this script then ensures the zips in all-zips are added to or
+# updated as would've happened had tek_survey.sh gotten these zips
+# as usual
+
+x=${TOP:="$HOME/code/tek_transparency"}
+
+ARCHIVE="$TOP/all-zips"
+
+for zipf in *.zip
+do
+	bn=`basename $zipf`
+	if [ ! -f $ARCHIVE/$bn ]
+	then
+		echo "will copy new $bn"
+		cp $zipf $ARCHIVE
+	elif ((`stat -c%s "$zipf"`>`stat -c%s "$ARCHIVE/$bn"`))
+	then
+		echo "will copy bigger $bn"
+		cp $zipf $ARCHIVE
+	fi
+done
