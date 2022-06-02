49a7d0c421275542ef28667eb137d38732ff3c69
sftcd <stephen.farrell@cs.tcd.ie>
2022-06-02 03:15:39 +0000

    fix shell oops

--- tek_survey.sh
+++ tek_survey.sh
@@ -1238,41 +1238,40 @@ do
         then
             echo "Estonia fail: not a zip ee-$midnight.txt"
             break
-        else
-            $CURL -L "$EE_BASE/$midnight" --output ee-$midnight.zip
-            if [[ $? == 0 ]]
+        fi
+        $CURL -L "$EE_BASE/$midnight" --output ee-$midnight.zip
+        if [[ $? == 0 ]]
+        then
+            # we do see zero sized files from .ee sometimes
+            # which is odd but whatever (could be their f/w
+            # doing that but what'd be the effect on the 
+            # app?) 
+            if [ ! -s ee-$midnight.zip ]
             then
-                # we do see zero sized files from .ee sometimes
-                # which is odd but whatever (could be their f/w
-                # doing that but what'd be the effect on the 
-                # app?) 
-                if [ ! -s ee-$midnight.zip ]
+                echo "Empty or non-existent downloaded Estonian file: ee-$midnight.zip ($fno)"
+            else
+                if [ ! -f $ARCHIVE/ee-$midnight.zip ]
                 then
-                    echo "Empty or non-existent downloaded Estonian file: ee-$midnight.zip ($fno)"
-                else
-                    if [ ! -f $ARCHIVE/ee-$midnight.zip ]
-                    then
-                        echo "New .ee file $fno ee-$midnight" 
-                        cp ee-$midnight.zip $ARCHIVE
-                    elif ((`stat -c%s "ee-$midnight.zip"`>`stat -c%s "$ARCHIVE/ee-$midnight.zip"`));then
-                        # if the new one is bigger than archived, then archive new one
-                        echo "Updated/bigger .ee file $fno ee-$midnight" 
-                        cp ee-$midnight.zip $ARCHIVE
-                    fi
-                    # try unzip and decode
-                    #if [[ "$DODECODE" == "yes" ]]
-                    #then
-                        #$UNZIP "ee-$midnight.zip" >/dev/null 2>&1
-                        #if [[ $? == 0 ]]
-                        #then
-                            #$TEK_DECODE >/dev/null
-                            #new_keys=$?
-                            #total_keys=$((total_keys+new_keys))
-                        #fi
-                        #rm -f export.bin export.sig
-                        #chunks_down=$((chunks_down+1))
-                    #fi
+                    echo "New .ee file $fno ee-$midnight" 
+                    cp ee-$midnight.zip $ARCHIVE
+                elif ((`stat -c%s "ee-$midnight.zip"`>`stat -c%s "$ARCHIVE/ee-$midnight.zip"`));then
+                    # if the new one is bigger than archived, then archive new one
+                    echo "Updated/bigger .ee file $fno ee-$midnight" 
+                    cp ee-$midnight.zip $ARCHIVE
                 fi
+                # try unzip and decode
+                #if [[ "$DODECODE" == "yes" ]]
+                #then
+                    #$UNZIP "ee-$midnight.zip" >/dev/null 2>&1
+                    #if [[ $? == 0 ]]
+                    #then
+                        #$TEK_DECODE >/dev/null
+                        #new_keys=$?
+                        #total_keys=$((total_keys+new_keys))
+                    #fi
+                    #rm -f export.bin export.sig
+                    #chunks_down=$((chunks_down+1))
+                #fi
             fi
         else
             echo "curl - error downloading ee-$midnight.zip (file $fno)"
