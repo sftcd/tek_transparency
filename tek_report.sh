8691519640949ccf71d3fa14a4ff7427f82c09cd
sftcd <stephen.farrell@cs.tcd.ie>
2022-06-06 13:53:12 +0000

    add CT link for .hr expired cert name

--- tek_report.sh
+++ tek_report.sh
@@ -324,7 +324,8 @@ Quoting some text from what's on that page now: "At most, Koronavilkku had 2.5 m
 appear to be new zip files there if one ignores the certificate (which I won't be doing:-). Presumably they'll fix
 that and it'll re-appear as an amber or green service. It's interesting though that that hasn't been fixed in 3 days - it'd
 seem to imply that either nobody's noticing that the service is down (i.e. no users?) or else that the client app isn't
-checking server certificates properly. Neither would seem good news for the GAEN approach.</li>
+checking server certificates properly. Neither would seem good news for the GAEN approach. The relevant CT log data can be seen
+at <a href="https://crt.sh/?q=en.apis-it.hr">https://crt.sh/?q=en.apis-it.hr</a>.</li>
     <li>20220604: Another certificate expiry! The server certificate the Gibraltar API server expired on June 4th 2022 (being instantiated on June 4th 2020 and valid for 
 two years). We'll see if that gets fixed or not I guess. For the record expired cert can be seen in CT logs here: 
 <a href="https://crt.sh/?q=app.beatcovid19.gov.gi">https://crt.sh/?q=app.beatcovid19.gov.gi</a>.</li>
